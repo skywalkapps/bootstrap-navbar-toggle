bda09b3344b6a9abfb0695f15915f74d0157a5c6
Martin Stanek <info@martinstanek.cz>
2016-04-01 15:59:13 +0200

    Clean up not used code

---
+++ docs/javascripts/bootstrap-navbar-toggle.js
@@ -0,0 +1,30 @@
+//
+// Scripts
+// -------------------
+
+// Required libraries
+// f.e. require('../../vendor/jquery/jquery.min');
+
+// Main scripts
+
+
+$(function(){
+
+  // Change
+  $('.navbar-toggle').on('click.bs.collapse', function(e){
+    if ($(this).data('label-expanded')) {
+      var $toggleLabel = $(this).find('.navbar-toggle-label')
+
+      if ($(this).hasClass('collapsed')) {
+        // Save current label
+        $(this).data('label-collapsed', $toggleLabel.text())
+        // Set new text
+        $toggleLabel.text($(this).data('label-expanded'))
+      }
+      else {
+        $toggleLabel.text($(this).data('label-collapsed'))
+      }
+    }
+  })
+
+});
